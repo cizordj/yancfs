4e9bc993bccf1fefbfe99af0abcb2f2999c1c858
cizordj <32869222+cizordj@users.noreply.github.com>
2022-08-18 19:35:19 -0300

    Create initial configuration

---
+++ init.lua
@@ -0,0 +1,27 @@
+-- mouse integration
+vim.g.mouse = 'n'
+-- dictionary completion
+vim.o.dictionary = "/usr/share/dict/brazilian,/usr/share/dict/american-english"
+
+-- colorscheme stuff
+vim.g.gruvbox_contrast_dark = 'hard'
+vim.g.gruvbox_contrast_light = 'hard'
+vim.g.gruvbox_italic = 1
+vim.cmd("colorscheme gruvbox")
+
+
+-- plugins configuration
+require("nvim-tree").setup({
+  sort_by = "case_sensitive",
+  view = {
+    adaptive_size = true,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
+
+-- keybingings
