ed7dca3f7e3b71842f66917bf8beb98013d6e15f
cizordj <32869222+cizordj@users.noreply.github.com>
2022-09-01 16:54:10 -0300

    Configure treesitter and lsp-config

--- init.lua
+++ init.lua
@@ -14,16 +14,16 @@ vim.cmd("colorscheme gruvbox")
 
 -- plugins configuration
 require("nvim-tree").setup({
-  sort_by = "case_sensitive",
-  view = {
-    adaptive_size = true,
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
+	sort_by = "case_sensitive",
+	view = {
+		adaptive_size = true,
+	},
+	renderer = {
+		group_empty = true,
+	},
+	filters = {
+		dotfiles = true,
+	},
 })
 
 -- keybingings >>>
@@ -31,18 +31,88 @@ vim.g.mapleader = ','
 
 -- window navigation
 vim.api.nvim_set_keymap('n', '<leader>v', '<cmd>vsplit<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>h', '<cmd>split<CR>', { noremap = true})
-vim.api.nvim_set_keymap('n', '<C-Up>', '<cmd>resize -1<CR>', { noremap = true})
-vim.api.nvim_set_keymap('n', '<C-Down>', '<cmd>resize +1<CR>', { noremap = true})
-vim.api.nvim_set_keymap('n', '<C-Left>', '<cmd>vertical resize -1<CR>', { noremap = true})
-vim.api.nvim_set_keymap('n', '<C-Right>', '<cmd>vertical resize +1<CR>', { noremap = true})
+vim.api.nvim_set_keymap('n', '<leader>h', '<cmd>split<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-Up>', '<cmd>resize -1<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-Down>', '<cmd>resize +1<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-Left>', '<cmd>vertical resize -1<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-Right>', '<cmd>vertical resize +1<CR>', { noremap = true })
 -- code manipulation
-vim.api.nvim_set_keymap('x', 'K', ":move '<-2<CR>gv-gv", { noremap = true})
-vim.api.nvim_set_keymap('x', 'J', ":move '>+1<CR>gv-gv", { noremap = true})
-vim.api.nvim_set_keymap('v', '<', '<gv', { noremap = true})
-vim.api.nvim_set_keymap('v', '>', '>gv', { noremap = true})
+vim.api.nvim_set_keymap('x', 'K', ":move '<-2<CR>gv-gv", { noremap = true })
+vim.api.nvim_set_keymap('x', 'J', ":move '>+1<CR>gv-gv", { noremap = true })
+vim.api.nvim_set_keymap('v', '<', '<gv', { noremap = true })
+vim.api.nvim_set_keymap('v', '>', '>gv', { noremap = true })
 
-vim.api.nvim_set_keymap('n', '<F2>', '<cmd>NvimTreeFindFileToggle<CR>', { noremap = true, desc = "See the current file in the file manager"})
-vim.api.nvim_set_keymap('n', '<F3>', '<cmd>NvimTreeToggle<CR>', { noremap = true, desc = "Open up the file manager"})
-vim.api.nvim_set_keymap('n', '<F4>', '<cmd>TagbarToggle<CR>', { noremap = true, desc = "Open up the tagbar"})
+vim.api.nvim_set_keymap('n', '<F2>', '<cmd>NvimTreeFindFileToggle<CR>',
+	{ noremap = true, desc = "See the current file in the file manager" })
+vim.api.nvim_set_keymap('n', '<F3>', '<cmd>NvimTreeToggle<CR>', { noremap = true, desc = "Open up the file manager" })
+vim.api.nvim_set_keymap('n', '<F4>', '<cmd>TagbarToggle<CR>', { noremap = true, desc = "Open up the tagbar" })
 -- <<< keybindings
+
+-- Treesitter
+require("nvim-treesitter.configs").setup({
+	ensure_installed = { "lua", "php" },
+	auto_install = true,
+	highlight = {
+		enable = true
+	}
+})
+
+-- LSP Config
+-- Mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+local opts = { noremap = true, silent = true }
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+	-- Enable completion triggered by <c-x><c-o>
+	vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+	-- Mappings.
+	-- See `:help vim.lsp.*` for documentation on any of the below functions
+	local bufopts = { noremap = true, silent = true, buffer = bufnr }
+	vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+	vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+	vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+	vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+	vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+	vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+	vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+	vim.keymap.set('n', '<space>wl', function()
+		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	end, bufopts)
+	vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+	vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+	vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+	vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+	vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+end
+
+local lsp_flags = {
+	debounce_text_changes = 150,
+}
+require('lspconfig')['phpactor'].setup {
+	on_attach = on_attach,
+	flags = lsp_flags,
+}
+require('lspconfig')['tsserver'].setup {
+	on_attach = on_attach,
+	flags = lsp_flags,
+}
+require('lspconfig')['sumneko_lua'].setup {
+	on_attach = on_attach,
+	flags = lsp_flags,
+	settings = {
+		globals = { "vim" }
+	},
+	workspace = {
+		library = vim.api.nvim_get_runtime_file("", true),
+	},
+	telemetry = {
+		enable = false,
+	}
+}
