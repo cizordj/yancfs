0358d12d657071f5d1b6ea6ca836deab0ad8dc9b
cizordj <32869222+cizordj@users.noreply.github.com>
2022-11-25 09:58:47 -0300

    Enable some extensions on phpactor

--- setup.sh
+++ setup.sh
@@ -1,14 +1,24 @@
 #!/bin/sh
 
-if command -v composer > /dev/null
+command -v composer > /dev/null && \
+	command -v php > /dev/null
+
+HAS_PHP_AND_COMPOSER=$?
+if [ $HAS_PHP_AND_COMPOSER ]
 then
-	if [ ! -d pack/plugins/opt/phpactor/vendor ]
-	then
-		cd pack/plugins/opt/phpactor
-		composer install --no-dev -o
-		cd "$OLDPWD"
-	fi
+	cd pack/plugins/opt/phpactor
+	composer install --no-dev -o
+	php bin/phpactor config:set language_server_phpstan.enabled true
+	php bin/phpactor config:set language_server_phpstan.level 9
+	php bin/phpactor config:set language_server_php_cs_fixer.enabled true
+	php bin/phpactor config:set symfony.enabled true
+	php bin/phpactor config:set language_server_code_transform.import_globals true
+	php bin/phpactor config:set code_transform.import_globals true
+	[ -d ~/.config/phpactor ] || mkdir ~/.config/phpactor
+	mv .phpactor.json ~/.config/phpactor/phpactor.json
+	cd "$OLDPWD"
 fi
+exit
 if command -v yarn > /dev/null
 then
 	yarn install
