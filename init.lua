237e4cf39a1c5418808b0d6f3b2e933057ae1bd3
cizordj <32869222+cizordj@users.noreply.github.com>
2022-12-01 09:20:36 -0300

    Require glow-hover only once
    - Remove warning about unused variable

--- init.lua
+++ init.lua
@@ -74,9 +74,11 @@ local lsp_flags = {
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
-    vim.cmd("packadd glow-hover.nvim")
-    require("glow-hover").setup()
+local on_attach = function(_, bufnr)
+    if nil == package.loaded["glow-hover"] then
+      vim.cmd("packadd glow-hover.nvim")
+      require("glow-hover").setup()
+    end
     -- Enable completion triggered by <c-x><c-o>
     vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
