dd9bdec7edbe141a56785ce06410f41bf01e826f
cizordj <32869222+cizordj@users.noreply.github.com>
2023-01-10 09:55:31 -0300

    Gänsemblünchem
    Ich spreche ein bisschen Deutsch!

--- readme.md
+++ readme.md
@@ -1,13 +1,17 @@
 # yancfs
 
-My personal NeoVim configuration written entirely from scratch using pure Lua
+Pronounced ```jˈæŋkˌɛfˈɛs```, yancfs is **Yet Another Neovim Configuration From Scratch**.
+
+This is my personal NeoVim configuration written entirely from scratch using pure Lua
 and no plugin manager whatsoever
 
 #### Usage
 
 - Clone this repo into `~/.config/nvim`
-- Initialize all submodules with `git submodule update --init --recursive`
-- Run the `setup` script and you're good to go.
+- Initialize all submodules with `./yancfs.sh init`
+- Install the modules' dependencies with `./yancfs.sh setup`
+
+Then you're good to go
 
 #### Update plugins
 
---
+++ yancfs.sh
@@ -0,0 +1,89 @@
+#!/bin/sh
+
+help() {
+        cat <<HELP
+Usage:
+./yancfs.sh <action>
+
+Where <action> can be any of these:
+
+init    -- Initialize all git submodules
+update  -- Update all git submodules
+setup   -- Install all modules dependencies
+HELP
+}
+
+version() {
+        cat <<HELP
+Yancfs v1.0
+HELP
+}
+
+init_modules() {
+        git submodule update --init --recursive
+}
+
+update_modules() {
+        git submodule update --recursive --remote
+}
+
+setup() {
+        command -v composer > /dev/null && \
+                command -v php > /dev/null
+
+        IT_HAS_PHP_AND_COMPOSER=$?
+        if [ $IT_HAS_PHP_AND_COMPOSER ]
+        then
+                cd pack/plugins/opt/phpactor
+                composer install --no-dev -o
+                php bin/phpactor config:set language_server_phpstan.enabled true
+                php bin/phpactor config:set language_server_phpstan.level 9
+                php bin/phpactor config:set language_server_php_cs_fixer.enabled true
+                php bin/phpactor config:set symfony.enabled true
+                php bin/phpactor config:set language_server_code_transform.import_globals true
+                php bin/phpactor config:set code_transform.import_globals true
+                [ -e ~/.config/phpactor/phpactor.json ] && rm ~/.config/phpactor/phpactor.json
+                [ -d ~/.config/phpactor ] || mkdir ~/.config/phpactor
+                mv .phpactor.json ~/.config/phpactor/phpactor.json
+                cd "$OLDPWD"
+                composer install --no-dev -o
+        fi
+
+        if command -v yarn > /dev/null
+        then
+                yarn install
+        fi
+}
+
+test ! $# -gt 0
+THERE_IS_NO_ARGUMENT=$?
+
+while true
+do
+        if [ $THERE_IS_NO_ARGUMENT -eq 0 ]; then
+                help
+                exit 0
+        elif [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+                help
+                exit 0
+        elif [ "$1" = "-v" ] || [ "$1" = "--version" ]; then
+                version
+                exit 0
+        elif [ "$1" = "init" ]; then
+                init_modules
+                exit 0
+        elif [ "$1" = "update" ]; then
+                update_modules
+                exit 0
+        elif [ "$1" = "setup" ]; then
+                setup
+                exit 0
+        elif [ $# -eq 1 ]; then
+                echo "Unknown action: $1"
+                exit 1
+                break
+        else
+                break
+        fi
+        shift 1
+done
