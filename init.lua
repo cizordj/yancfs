152ccc8ec74f12914609ffc47c67304ac7702913
cizordj <32869222+cizordj@users.noreply.github.com>
2023-02-12 21:19:17 -0300

    Remove the deprecated sumneko_lua configuration

--- init.lua
+++ init.lua
@@ -144,7 +144,7 @@ require('lspconfig')['tsserver'].setup {
 --     flags = lsp_flags,
 --     on_attach = on_attach
 -- }
-require('lspconfig')['sumneko_lua'].setup {
+require('lspconfig')['lua_ls'].setup {
     on_attach = on_attach,
     flags = lsp_flags,
     settings = {
