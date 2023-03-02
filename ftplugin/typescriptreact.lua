84e624d60fbc2e621392f705cd3be036c1f6dca4
cizordj <32869222+cizordj@users.noreply.github.com>
2023-03-02 07:39:31 -0300

    Use autopairs on reactjs files

--- ftplugin/typescriptreact.lua
+++ ftplugin/typescriptreact.lua
@@ -6,3 +6,7 @@ local node_binaries_path = vim.fn.getcwd() .. '/node_modules/.bin'
 if vim.fn.isdirectory(node_binaries_path) then
     vim.env.PATH = vim.env.PATH .. ':' .. node_binaries_path
 end
+if nil == package.loaded["nvim-autopairs"] then
+  vim.cmd("packadd nvim-autopairs")
+  require("nvim-autopairs").setup {}
+end
