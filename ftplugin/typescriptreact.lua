2db78c69f36b7a02d4214999d5cb76d43ede5b9f
cizordj <32869222+cizordj@users.noreply.github.com>
2023-03-02 10:40:30 -0300

    Comment it out for now

--- ftplugin/typescriptreact.lua
+++ ftplugin/typescriptreact.lua
@@ -2,11 +2,12 @@ vim.bo.shiftwidth = 2
 vim.bo.tabstop = 2
 vim.bo.expandtab = true
 
-local node_binaries_path = vim.fn.getcwd() .. '/node_modules/.bin'
-local has_been_added = string.find(vim.env.PATH, node_binaries_path)
-if vim.fn.isdirectory(node_binaries_path) & false == has_been_added then
-    vim.env.PATH = vim.env.PATH .. ':' .. node_binaries_path
-end
+-- local node_binaries_path = vim.fn.getcwd() .. '/node_modules/.bin'
+-- local has_been_added = string.find(vim.env.PATH, node_binaries_path)
+-- if 1 == vim.fn.isdirectory(node_binaries_path) and nil == has_been_added then
+--     vim.notify('Configurei o diretório')
+--     vim.env.PATH = vim.env.PATH .. ':' .. node_binaries_path
+-- end
 if nil == package.loaded["nvim-autopairs"] then
   vim.cmd("packadd nvim-autopairs")
   require("nvim-autopairs").setup {}
