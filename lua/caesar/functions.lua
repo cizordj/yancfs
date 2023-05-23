fd19ed624502cb9bbb9a5e8819fe7e5f70e84e46
cizordj <32869222+cizordj@users.noreply.github.com>
2023-05-23 09:40:25 -0300

    Fix the font in neovide

--- lua/caesar/functions.lua
+++ lua/caesar/functions.lua
@@ -76,7 +76,7 @@ function M:setupNeovide()
       "ripple",
       "wireframe"
   }
-  vim.g.guifont = "JetBrainsMono Nerd Font:h20"
+  vim.o.guifont = "JetBrainsMono Nerd Font:h20"
   local i = math.random(0, #(cursor_particles) - 1)
   -- vim.g.neovide_cursor_vfx_mode = cursor_particles[i];
   vim.cmd('let g:neovide_cursor_vfx_mode = "' .. cursor_particles[i] .. '"')
