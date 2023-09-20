283bf5a6a70dd048e053108ad2a62b153f49e063
cizordj <32869222+cizordj@users.noreply.github.com>
2023-09-20 08:14:09 -0300

    Fix dad-bod completion

--- lua/caesar/functions.lua
+++ lua/caesar/functions.lua
@@ -57,6 +57,7 @@ function M:loadUpDadbod()
   vim.cmd("packadd vim-dadbod-ui")
   vim.cmd("packadd vim-dadbod-completion")
   vim.api.nvim_set_keymap('n', '<F5>', "<cmd>DBUIToggle<cr>", { noremap = true, desc = "Open up the database viewer" })
+  vim.cmd("helptags ALL")
   vim.cmd("DBUIToggle")
 end
 
