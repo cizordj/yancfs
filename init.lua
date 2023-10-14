3d6b3092f7e17b542a529d83e004428dc7b9242c
cizordj <32869222+cizordj@users.noreply.github.com>
2023-10-13 23:51:49 -0300

    Disable some stuff temporarily

--- init.lua
+++ init.lua
@@ -197,13 +197,13 @@ require('lspconfig')['tailwindcss'].setup {
 local null_ls = require("null-ls")
 null_ls.setup({
     sources = {
-        null_ls.builtins.formatting.prettier.with({
-            command = { scriptpath .. "/node_modules/.bin/prettier" },
-            extra_filetypes = { "php", "html" },
-            disabled_filetypes = { "markdown" }
-        }),
+        -- null_ls.builtins.formatting.prettier.with({
+        --     command = { scriptpath .. "/node_modules/.bin/prettier" },
+        --     extra_filetypes = { "php", "html" },
+        --     disabled_filetypes = { "markdown" }
+        -- }),
         null_ls.builtins.diagnostics.shellcheck,
-        null_ls.builtins.diagnostics.phpcs,
+        -- null_ls.builtins.diagnostics.phpcs,
         null_ls.builtins.code_actions.shellcheck,
         null_ls.builtins.diagnostics.php.with({
             command = {
